e21f76f286aa6a8ccd73eb5281c4ca2dac7a8128
Isaque-Evaristo <isaqueevaristo39@gmail.com>
2022-05-16 17:17:41 -0300

    trabalho2pontos

---
+++ isaque/trabalho 9/main.js
@@ -0,0 +1,7 @@
+function ok(){
+    alert("me dá logo os pontos")
+    var num1 = document.getElementById('num1').value
+    var num2 = document.getElementById('num2').value
+    var num3 = document.getElementById('num3').value
+    prompt("cade meus pontos?")
+}
